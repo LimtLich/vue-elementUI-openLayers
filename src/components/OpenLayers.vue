71dfc616cdb90054aee317023a66c0e559a17e04
limt <921136839@qq.com>
2017-12-14 10:58:21 +0800

    init openlayers

---
+++ src/components/OpenLayers.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="map" id="map"></div>
+</template>
+
+<script>
+import Map from "ol/map";
+import View from "ol/view";
+import TileLayer from "ol/layer/tile";
+import XYZ from "ol/source/xyz";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  mounted: function() {
+    var map = new Map({
+      target: document.getElementById("map"),
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
